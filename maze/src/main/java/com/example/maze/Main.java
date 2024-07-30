79393a3a2ee6268ecff5905d93ccac5929a29a3d
yusay1498 <u.yusay1498@gmail.com>
2024-07-30 13:12:43 +0900

    深さ優先探索での経路探索

--- maze/src/main/java/com/example/maze/Main.java
+++ maze/src/main/java/com/example/maze/Main.java
@@ -1,6 +1,7 @@
 package com.example.maze;
 
 import java.util.Random;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,20 +31,19 @@ public class Main {
         System.out.println();
 
         // TODO: (1) 迷路作成
-        int[][] num;
-        num = new int[h][w];
+        int[][] maze = new int[h][w];
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 if (i == 0 || i == h - 1) {
-                    num[i][j] = 1;
+                    maze[i][j] = 1;
                 } else {
                     if (j == w - 1) {
-                        num[i][j] = 1;
+                        maze[i][j] = 1;
                     } else if (j == 0 || (i % 2 == 0 && j % 2 == 0)) {
-                        num[i][j] = 1;
+                        maze[i][j] = 1;
                     } else {
-                        num[i][j] = 0;
+                        maze[i][j] = 0;
                     }
                 }
             }
@@ -57,69 +57,32 @@ public class Main {
                     Random rand = new Random();
                     int n = rand.nextInt(3);
                     if (n == 0) {
-                        if (num[i][j - 1] == 1) {
-                            num[i + 1][j] = 1;
+                        if (maze[i][j - 1] == 1) {
+                            maze[i + 1][j] = 1;
                         } else {
-                            num[i][j - 1] = 1;
+                            maze[i][j - 1] = 1;
                         }
-                    } else if (n == 1 && num[i - 1][j] == 0) {
-                        if (num[i - 1][j] == 1) {
-                            num[i][j + 1] = 1;
+                    } else if (n == 1 && maze[i - 1][j] == 0) {
+                        if (maze[i - 1][j] == 1) {
+                            maze[i][j + 1] = 1;
                         } else {
-                            num[i - 1][j] = 1;
+                            maze[i - 1][j] = 1;
                         }
                     } else if (n == 2) {
-                        num[i][j + 1] = 1;
+                        maze[i][j + 1] = 1;
                     } else {
-                        num[i + 1][j] = 1;
+                        maze[i + 1][j] = 1;
                     }
                 }
             }
         }
 
-        num[1][0] = 0;
-        num[h - 2][w - 1] = 3;
+        maze[1][0] = 0;
+        maze[h - 2][w - 1] = 3;
 
-        int ii = 1;
-        int jj = 0;
         // TODO: (2) ルート作成
-        ForwardWidth fw = new ForwardWidth();
-        BackWidth bw = new BackWidth();
-        ForwardHeight fh = new ForwardHeight();
-        BackHeight bh = new BackHeight();
 
-        int[][] num2 = fw.forwardWidth(num, ii, jj);
-        ii = fw.getI();
-        System.out.println(ii);
-        jj = fw.getJ();
-        System.out.println(jj);
-
-        if (num2[ii - 1][jj] != 1) {
-            num2 = bh.backHeight(num2, ii, jj);
-            ii = bh.getI();
-            System.out.println(ii);
-            jj = bh.getJ();
-            System.out.println(jj);
-        } else if (num2[ii][jj - 1] != 1) {
-            num2 = bw.backWidth(num2, ii, jj);
-            ii = bw.getI();
-            System.out.println(ii);
-            jj = bw.getJ();
-            System.out.println(jj);
-        } else if (num2[ii + 1][jj] != 1) {
-            num2 = fh.forwardHeight(num2, ii, jj);
-            ii = fh.getI();
-            System.out.println(ii);
-            jj = fh.getJ();
-            System.out.println(jj);
-        } else {
-            num2 = fw.forwardWidth(num2, ii, jj);
-            ii = fw.getI();
-            System.out.println(ii);
-            jj = fw.getJ();
-            System.out.println(jj);
-        }
-
+        maze = solveMaze(maze, 1, 0);
 
         // 迷路・ルート描画に使用する文字を定義
         String WALL = "██";
@@ -129,13 +92,13 @@ public class Main {
         // TODO: (1) 迷路描画 -> (2) 迷路＆ルート描画
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                if (num2[i][j] == 0) {
+                if (maze[i][j] == 0) {
                     if (i == h - 2 && j == w - 1) {
                         System.out.println(PASSAGE);
                     } else {
                         System.out.print(PASSAGE);
                     }
-                } else if (num2[i][j] == 1) {
+                } else if (maze[i][j] == 1) {
                     if (j == w - 1) {
                         System.out.println(WALL);
                     } else {
@@ -152,35 +115,34 @@ public class Main {
         }
     }
 
-//    public static int[][] forwardWidth(int[][] maze, int i, int j) {
-//        while (maze[i][j] != 1 && maze[i - 1][j] == 1) {
-//            maze[i][j] = 2;
-//            j += 1;
-//        }
-//        return maze;
-//    }
-//
-//    public static int[][] backWidth(int[][] maze, int i, int j) {
-//        while (maze[i][j] != 1 && maze[i + 1][j] == 1) {
-//            maze[i][j] = 2;
-//            j -= 1;
-//        }
-//        return maze;
-//    }
-//
-//    public static int[][] forwardHeight(int[][] maze, int i, int j) {
-//        while (maze[i][j] != 1 && maze[i][j - 1] == 1) {
-//            maze[i][j] = 2;
-//            i += 1;
-//        }
-//        return maze;
-//    }
-//
-//    public static int[][] backHeight(int[][] maze, int i, int j) {
-//        while (maze[i][j] != 1 && maze[i][j + 1] == 1) {
-//            maze[i][j] = 2;
-//            i -= 1;
-//        }
-//        return maze;
-//    }
+    private static int[][] solveMaze(int[][] maze, int startI, int startJ) {
+        int[][] solvedMaze = new int[maze.length][maze[0].length];
+        for (int i = 0; i < maze.length; i++) {
+            System.arraycopy(maze[i], 0, solvedMaze[i], 0, maze[i].length);
+        }
+
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{startI, startJ});
+
+        while (!stack.isEmpty()) {
+            int[] pos = stack.pop();
+            int i = pos[0], j = pos[1];
+
+            if (solvedMaze[i][j] == 3) {
+                solvedMaze[i][j] = 2;
+                return solvedMaze;
+            }
+
+            if (solvedMaze[i][j] != 2) {
+                solvedMaze[i][j] = 2;
+
+                if (i > 0 && (solvedMaze[i - 1][j] == 0 || solvedMaze[i - 1][j] == 3)) stack.push(new int[]{i - 1, j});
+                if (j > 0 && (solvedMaze[i][j - 1] == 0 || solvedMaze[i][j - 1] == 3)) stack.push(new int[]{i, j - 1});
+                if (i < maze.length - 1 && (solvedMaze[i + 1][j] == 0 || solvedMaze[i + 1][j] == 3)) stack.push(new int[]{i + 1, j});
+                if (j < maze[0].length - 1 && (solvedMaze[i][j + 1] == 0 || solvedMaze[i][j + 1] == 3)) stack.push(new int[]{i, j + 1});
+            }
+        }
+
+        return solvedMaze;
+    }
 }
