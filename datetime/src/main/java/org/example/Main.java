80a0e3109f3b87012465dc78721a8ed9e4be1327
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-01 15:18:39 +0900

    無駄ファイル削除

--- datetime/src/main/java/org/example/Main.java
+++ datetime/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +19,14 @@ public class Main {
         LocalDateTime dateTime = LocalDateTime.of(2024, 8, 1, 15, 2);
         System.out.println(dateTime);
 
+        Date legcyDate = new Date(2024, 8, 1, 15, 2);
+        System.out.println(legcyDate);
+        legcyDate = new Date(2024, Calendar.AUGUST, 1, 15, 2);
+        System.out.println(legcyDate);
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(legcyDate);
+        calendar.add(Calendar.HOUR_OF_DAY, 12);
+        System.out.println(calendar.getTime());
     }
 }
