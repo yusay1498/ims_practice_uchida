f0c43e70ea2a580be8f8d96a6c91d8f8b0132ae4
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-02 10:35:44 +0900

    gitのリポジトリが見つからないらしい

--- freeAssignment/wordChain/src/main/java/org/example/Main.java
+++ freeAssignment/wordChain/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Path dictionaryPath = Path.of("resources/dictionary.txt");
+        System.out.println(Files.exists(dictionaryPath));
     }
 }
