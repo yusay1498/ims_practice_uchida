79150d368e6f7d33c8c943e6e57aa0edbf711d96
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-14 15:48:22 +0900

    Paizaの練習用のディレクトリ

---
+++ paiza_training/scannerUser/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        List user = List.of(
+                scanner.next()
+        );
+    }
+}
