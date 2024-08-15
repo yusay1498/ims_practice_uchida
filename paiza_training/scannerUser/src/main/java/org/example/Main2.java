fdae1125644d79ba2261b5c8166ffb0cf80a201b
YuseiUchida <68174245+yusay1498@users.noreply.github.com>
2024-08-15 11:05:14 +0900

    Update Main2.java

--- paiza_training/scannerUser/src/main/java/org/example/Main2.java
+++ paiza_training/scannerUser/src/main/java/org/example/Main2.java
@@ -16,26 +16,19 @@ public class Main2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        scanner.nextLine();  // Consume newline left-over
         int age = scanner.nextInt();
-        scanner.nextLine();  // Consume newline left-over
         List<Map<String, String>> users = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             Map<String, String> user = new HashMap<>();
-            user.put("nickname", scanner.nextLine());
-            user.put("old", scanner.nextLine());
-            user.put("birth", scanner.nextLine());
-            user.put("state", scanner.nextLine());
+            user.put("nickname", scanner.next());
+            user.put("old", scanner.next());
+            user.put("birth", scanner.next());
+            user.put("state", scanner.next());
             users.add(user);
         }
         for (Map<String, String> user : users) {
             if (user.get("old").equals(String.valueOf(age))) {
-                System.out.println("User{");
-                System.out.println("nickname : " + user.get("nickname"));
-                System.out.println("old : " + user.get("old"));
-                System.out.println("birth : " + user.get("birth"));
-                System.out.println("state : " + user.get("state"));
-                System.out.println("}");
+                System.out.println(user.get("nickname"));
             }
         }
     }
