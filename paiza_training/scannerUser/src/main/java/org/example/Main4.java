c9fb8c57a8269cc1376e40e0ee742fee38865c3a
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-15 11:38:13 +0900

    STEP4終了

--- paiza_training/scannerUser/src/main/java/org/example/Main4.java
+++ paiza_training/scannerUser/src/main/java/org/example/Main4.java
@@ -2,41 +2,28 @@ package org.example;
 
 import java.util.*;
 
-//user数　更新回数
-//userの情報
-//userの番号 新しいuserの名前
-
-//入力例
-//3 2
-//mako 13 08/08 nara
-//taisei 16 12/04 nagano
-//megumi 14 11/02 saitama
-//2 taihei
-//3 megu
-
-//出力例
-//mako 13 08/08 nara
-//taihei 16 12/04 nagano
-//megu 14 11/02 saitama
-
 public class Main4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = Integer.parseInt(scanner.next());
-        int M = Integer.parseInt(scanner.nextLine());
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
         List<Map<String, Object>> users = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             Map<String, Object> user = new HashMap<>();
-            user.put("nickname", scanner.next());
-            user.put("old", Integer.parseInt(scanner.next()));
-            user.put("birth", scanner.next());
-            user.put("state", scanner.next());
+            String[] userInfo = scanner.nextLine().split(" ");
+            user.put("nickname", userInfo[0]);
+            user.put("old", Integer.parseInt(userInfo[1]));
+            user.put("birth", userInfo[2]);
+            user.put("state", userInfo[3]);
             users.add(user);
         }
 
         for (int i = 0; i < M; i++) {
-            int K = Integer.parseInt(scanner.next());
-            users.get(K + 1).get("nickname") = scanner.next();
+            String[] updateInfo = scanner.nextLine().split(" ");
+            int K = Integer.parseInt(updateInfo[0]) - 1;
+            String newName = updateInfo[1];
+            users.get(K).put("nickname", newName);
         }
 
         for (Map<String, Object> user : users) {
