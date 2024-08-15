36099fb4b5819e71490428bcdeea75ee717a0f2c
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-15 10:27:45 +0900

    一旦

--- paiza_training/scannerUser/src/main/java/org/example/Main.java
+++ paiza_training/scannerUser/src/main/java/org/example/Main.java
@@ -1,15 +1,22 @@
 package org.example;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        List user = List.of(
-                scanner.next()
-        );
+        Map<String, String> user = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+             user = Map.of(
+                    "nickname" , scanner.next(),
+                    "old", scanner.next(),
+                    "birth", scanner.next(),
+                    "state", scanner.next()
+            );
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.println(user.get(i));
+        }
     }
 }
