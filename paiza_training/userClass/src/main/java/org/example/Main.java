171063b4afe8e4f8a6c48080cb3b7804fa08c86e
YuseiUchida <68174245+yusay1498@users.noreply.github.com>
2024-08-15 12:00:40 +0900

    Update Main.java

--- paiza_training/userClass/src/main/java/org/example/Main.java
+++ paiza_training/userClass/src/main/java/org/example/Main.java
@@ -8,24 +8,25 @@ public class Main {
         int N = scanner.nextInt();
         List<Map<String, Object>> users = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            Map<String, Object> user = new HashMap<>();
-            if (scanner.next().equals("make")) {
+            String command = scanner.next();
+            if (command.equals("make")) {
+                Map<String, Object> user = new HashMap<>();
                 user.put("num", scanner.nextInt());
                 user.put("name", scanner.next());
                 users.add(user);
-            } else if (scanner.next().equals("getname")) {
-                getName(scanner.nextInt(), users);
-            } else if (scanner.next().equals("getnum")) {
-                getNum(scanner.next(), users);
+            } else if (command.equals("getname")) {
+                getNameByNum(scanner.nextInt(), users);
+            } else if (command.equals("getnum")) {
+                getNum(scanner.nextInt(), users);
             }
         }
     }
 
-    public void getName(int n , List<Map<String, Object>> users) {
+    public static void getNameByNum(int n , List<Map<String, Object>> users) {
         System.out.println(users.get(n).get("name"));
     }
 
-    private void getNum(int n , List<Map<String, Object>> users) {
+    public static void getNum(int n , List<Map<String, Object>> users) {
         System.out.println(users.get(n).get("num"));
     }
 }
