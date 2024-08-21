681c1b446734031c20424caae03ac812cf4c4c09
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-21 15:13:28 +0900

    変更

---
+++ trainingIMDL/demo_jdbc/src/test/java/com/example/demo_jdbc/JdbcCardRepositoryTests.java
@@ -0,0 +1,32 @@
+package com.example.demo_jdbc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+public class JdbcCardRepositoryTests {
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName
+            .parse("postgres"));
+
+    @BeforeAll
+    static void startContainers() {
+        postgreSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+        registry.add("spring.sql.init.mode", () -> "always");
+    }
+}
