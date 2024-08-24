c756fe73e881935ff81282a3290615244fd8af64
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-24 12:24:27 +0900

    STEP1終了

---
+++ paiza_training/increment/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + 1);
+    }
+}
