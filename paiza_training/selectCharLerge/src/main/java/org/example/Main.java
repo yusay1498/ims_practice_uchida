240ffe72426deb96cc74328461b4d46d97d485fc
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-24 19:37:47 +0900

    STEP1終了

---
+++ paiza_training/selectCharLerge/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input.toUpperCase());
+    }
+}
