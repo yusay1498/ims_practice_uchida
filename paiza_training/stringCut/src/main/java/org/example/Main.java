177f8e46d7142d5a32c052765367ec3b0c2b05dd
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-24 19:20:29 +0900

    STEP1終了

---
+++ paiza_training/stringCut/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+    }
+}
