a4516d686230d7f7737d9b47bfc595e80927087f
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-08-26 12:52:48 +0900

    STEP3終了

---
+++ paiza_training/gomoku/src/main/java/org/example/Main3.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 5;
+        String[] matrix = new String[n];
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            matrix[i] = input;
+        }
+
+        int counterO = 0;
+        int counterX = 0;
+        boolean judge = false;
+
+        // 入力文字列をループしてカウント
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < matrix[i].length(); j++) {
+                if (matrix[i].charAt(j) == 'O') {
+                    counterO++;
+                } else if (matrix[i].charAt(j) == 'X') {
+                    counterX++;
+                }
+            }
+            if (counterO == 5 || counterX == 5) break;
+            else {
+                counterX = 0;
+                counterO = 0;
+            }
+        }
+
+        // 出力条件の判定
+        if (counterO == 5) {
+            System.out.println("O");
+        } else if (counterX == 5) {
+            System.out.println("X");
+        } else {
+            System.out.println("D");
+        }
+    }
+}
