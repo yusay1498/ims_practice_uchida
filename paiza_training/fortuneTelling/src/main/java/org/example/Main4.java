849dbe6e2361e3db906e79567b16b4ae9790237a
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-09-13 11:47:48 +0900

    STEP5終了

--- paiza_training/fortuneTelling/src/main/java/org/example/Main4.java
+++ paiza_training/fortuneTelling/src/main/java/org/example/Main4.java
@@ -5,19 +5,37 @@ import java.util.*;
 public class Main4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String T = scanner.next();
+        String U = scanner.next();
+        int n = scanner.nextInt();
+        List<Map<String, String>> users = new ArrayList<Map<String, String>>();
+
+        for (int i = 0; i < n; i++) {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put(scanner.next(), scanner.next());
+            users.add(map);
+        }
+
         int m = scanner.nextInt();
-        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
 
         for (int i = 0; i < m; i++) {
             Map<String, String> map = new HashMap<String, String>();
             map.put(scanner.next(), scanner.next());
-            list.add(map);
+            result.add(map);
         }
 
-        for (Map<String, String> map : list) {
-            if (map.containsKey(T)) {
-                System.out.println(map.get(T));
+        String blood = null;
+        for (Map<String, String> map : users) {
+            if (map.containsKey(U)) {
+                blood = map.get(U);
+                break;
+            }
+        }
+
+        for (Map<String, String> map : result) {
+            if (map.containsKey(blood)) {
+                System.out.println(map.get(blood));
                 break;
             }
         }
