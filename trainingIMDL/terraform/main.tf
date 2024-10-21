faff164c767e995a9c8b84ab884041f99684a054
yusay1498 <uchida_yusei@ims-sol.co.jp>
2024-10-21 15:43:21 +0900

    terraformの触りのところまで保存

---
+++ trainingIMDL/terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "local_file" "foo" {
+  filename = "/home/ubuntu/foo.txt"
+  content  = "bar"
+}
